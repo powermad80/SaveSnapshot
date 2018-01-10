00a403265397464035daa4c8e729541c0301c976
Jake <jakedubusker@gmail.com>
2018-01-09 20:30:43 -0500

    Initial commit

---
+++ VisualSaveManager/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace VisualSaveManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.delBackup = new System.Windows.Forms.Button();
+            this.delGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Restore";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Backup Name";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(94, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Select Backup";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 72);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(260, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "Select Game";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Add Game";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Game Name";
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // delBackup
+            // 
+            this.delBackup.Location = new System.Drawing.Point(278, 45);
+            this.delBackup.Name = "delBackup";
+            this.delBackup.Size = new System.Drawing.Size(75, 23);
+            this.delBackup.TabIndex = 7;
+            this.delBackup.Text = "Delete";
+            this.delBackup.UseVisualStyleBackColor = true;
+            this.delBackup.Click += new System.EventHandler(this.delBackup_Click);
+            // 
+            // delGame
+            // 
+            this.delGame.Location = new System.Drawing.Point(278, 72);
+            this.delGame.Name = "delGame";
+            this.delGame.Size = new System.Drawing.Size(75, 23);
+            this.delGame.TabIndex = 8;
+            this.delGame.Text = "Delete";
+            this.delGame.UseVisualStyleBackColor = true;
+            this.delGame.Click += new System.EventHandler(this.delGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 134);
+            this.Controls.Add(this.delGame);
+            this.Controls.Add(this.delBackup);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Save Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button delBackup;
+        private System.Windows.Forms.Button delGame;
+    }
+}
+
---
+++ VisualSaveManager/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace VisualSaveManager
+{
+    public partial class Form1 : Form
+    {
+        public string selectedSave;
+        public string paths = "~/backups/";
+        public string gameDir;
+        //IList<string> saveList = new List<string>();
+        //IList<string> gameList = new List<string>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Select Game")
+            {
+                MessageBox.Show("You must select a game");
+                return;
+            }
+            string game = comboBox2.Text;
+            string name = textBox1.Text;
+            bool alreadyExists = false;
+            var fS = new FileStream("~/backups/" + game + '/' + "saves.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fS, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (name == line)
+                    {
+                        alreadyExists = true;
+                    }
+                }
+            }
+
+            if (!alreadyExists)
+            {
+                using (StreamWriter file =
+                    new StreamWriter("~/backups/" + game + '/' + "saves.txt", true))
+                {
+                    file.WriteLine(name);
+                }
+            }
+
+            Directory.CreateDirectory("~/backups/" + game + '/' + name + '/');
+            DirectoryCopyExample.DirectoryCopy(gameDir, "~/backups/" + game + '/' + name + '/', true);
+            if (!alreadyExists)
+                comboBox1.Items.Add(name);
+            MessageBox.Show("Success!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Select Game")
+            {
+                MessageBox.Show("You must select a game");
+                return;
+            }
+
+            if (comboBox1.Text == null)
+            {
+                MessageBox.Show("Select a backup to restore.");
+                return;
+            }
+
+            string game = comboBox2.Text;
+            string name = comboBox1.Text;
+            DirectoryCopyExample.DirectoryCopy("~/backups/" + game + '/' + name + '/', gameDir, true);
+            MessageBox.Show("Success!");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedSave = comboBox1.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == -1)
+                return;
+
+            var fS = new FileStream("games.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fS, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (comboBox2.Text == line)
+                    {
+                        gameDir = streamReader.ReadLine();
+                        if (!Directory.Exists(gameDir))
+                        {
+                            gameDir = null;
+                            comboBox2.SelectedIndex = -1;
+                            MessageBox.Show("Game save folder not found. Has it moved, or is it located on a removed drive?");
+                            return;
+                        }
+                    }
+
+                }
+                loadList(comboBox1, "~/backups/" + comboBox2.Text + "/saves.txt");
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadList(comboBox2, "games.txt");
+        }
+
+        public void loadList(ComboBox lst, string file)
+        {
+            lst.Items.Clear();
+            string line;
+            StreamReader readFile = new StreamReader(file);
+            while ((line = readFile.ReadLine()) != null)
+            {
+                if ((line[0] != '/') && (line[1] != ':'))
+                    lst.Items.Add(line);
+            }
+            readFile.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog fbd = new CommonOpenFileDialog();
+            fbd.IsFolderPicker = true;
+            if (fbd.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                if (GetDirSize(fbd.FileName) > 5000000)
+                {
+                    var confirmResult = MessageBox.Show("Directory size exceeds 50MB, save directories are usually much smaller. Proceeding with backup may take a while, continue anyway?", "Large Directory Detected", MessageBoxButtons.OKCancel);
+                    if (confirmResult == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                }
+                string result = fbd.FileName;
+                comboBox2.Items.Add(textBox2.Text);
+                using (StreamWriter file = new StreamWriter("games.txt", true))
+                {
+                    file.WriteLine(textBox2.Text);
+                    file.WriteLine(result);
+                }
+                Directory.CreateDirectory("~/backups/" + textBox2.Text + "/");
+                File.Create("~/backups/" + textBox2.Text + "/saves.txt").Close();
+                MessageBox.Show("Success!");
+            }
+            else
+            {
+                return;
+            }
+            fbd.Dispose();
+        }
+
+        private void delBackup_Click(object sender, EventArgs e)
+        {
+            DialogResult cont = MessageBox.Show("Are you sure you want to delete the selected backup?", "Warning", MessageBoxButtons.YesNo);
+
+            if (cont == DialogResult.No)
+            {
+                return;
+            }
+            else if (cont == DialogResult.Yes)
+            {
+                string game = comboBox2.Text;
+                string name = comboBox1.Text;
+                Directory.Delete("~/backups/" + game + '/' + name + '/', true);
+
+                string[] savefile = File.ReadAllLines("~/backups/" + game + '/' + "saves.txt");
+                using (StreamWriter f = new StreamWriter("~/backups/" + game + '/' + "saves.txt"))
+                {
+                    foreach (string s in savefile)
+                    {
+                        if (s != name)
+                            f.WriteLine(s);
+                    }
+                }
+                comboBox1.Items.Remove(name);
+                savefile = null;
+                MessageBox.Show("Backup deleted.");
+            }
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text == "Backup Name")
+                textBox1.Text = "";
+        }
+
+        private void delGame_Click(object sender, EventArgs e)
+        {
+            DialogResult cont = MessageBox.Show("Are you sure you want to delete all this game's backups?", "Warning", MessageBoxButtons.YesNo);
+            if (cont == DialogResult.No)
+            {
+                return;
+            }
+            else if (cont == DialogResult.Yes)
+            {
+                string game = comboBox2.Text;
+                bool skip = false;
+                Directory.Delete("~/backups/" + game + '/', true);
+
+                string[] gamefile = File.ReadAllLines("games.txt");
+                using (StreamWriter f = new StreamWriter("games.txt"))
+                {
+                    foreach (string s in gamefile)
+                    {
+                        if (s == game)
+                        {
+                            skip = true;
+                        }
+                        else if (!skip)
+                        {
+                            f.WriteLine(s);
+                            skip = false;
+                        }
+                    }
+                }
+                gamefile = null;
+                comboBox2.Items.Remove(game);
+                MessageBox.Show("All backups deleted.");
+            }
+
+
+        }
+
+        public static long GetDirSize(string dir)
+        {
+            DirectoryInfo d = new DirectoryInfo(dir);
+            return DirSize(d);
+        }
+
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox2.Text == "Game Name")
+                textBox2.Text = "";
+        }
+    }
+}
